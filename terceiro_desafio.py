4380c649514b9ed9da204f2fb77e675f259504cb
Rigby3ip <gutocesar.c1@gmail.com>
2024-03-16 17:14:09 -0300

    segundo commit

---
+++ terceiro_desafio.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request, make_response
+from segundo_desafio import personagens
+
+app = Flask(__name__)
+
+@app.route('/characters/', methods=['POST'])
+def criar_personagem():
+    data = request.json
+    personagens.append(data)
+    return jsonify(data), 201
+
+@app.route('/characters/', methods=['GET'])
+def ver_personagens():
+    return make_response(jsonify(personagens), 200)
+
+if __name__ == '__main__':
+    app.run(debug=True)
